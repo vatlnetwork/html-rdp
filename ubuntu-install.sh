61388061a700bceb0cb1fa3a2c439e8a79a71e3b
John Gustafson <vatlnetwork@gmail.com>
2026-01-30 17:18:56 -0700

    add more automation to the script

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -2,15 +2,21 @@
 
 # install docker
 curl -fsSL https://get.docker.com | sh
-sudo apt install uidmap
+sudo apt install uidmap -y
 dockerd-rootless-setuptool.sh install
 
 # setup guacamole server
 docker compose up -d
 
 # install dependencies for backend
-sudo apt install openjdk-21-jdk
-sudo apt install maven
+sudo apt install openjdk-21-jdk -y
+sudo apt install maven -y
+
+# get ip and prompt user
+sudo apt install net-tools -y
+clear
+ifconfig
+read -p "Please copy down the IP above. Press enter when you are ready to continue."
 
 # compile & start the backend
 cd backend
